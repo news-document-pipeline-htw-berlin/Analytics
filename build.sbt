d1fcc88f2ce61596d9e81cd7681ed9a5def1fc0c
Lennart Döring <doering@iplytics.com>
2020-12-03 08:42:19 +0100

    sentinment import Map

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "spark_nlp"
+name := "Analytics"
 
 version := "0.1"
 
--- src/main/scala/Sentiment_Analysis.scala
+++ src/main/scala/Sentiment_Analysis.scala
@@ -2,17 +2,29 @@ import org.apache.spark.sql.DataFrame
 
 class Sentiment_Analysis(sentPath: String) {
 
- val sentiments: Map[String, Int] = fileLoader(sentPath)
+ val sentiments: Map[String, Double] = fileLoader(sentPath)
 
- def fileLoader(path: String): Map[String, Int] ={
+ def fileLoader(path: String): Map[String, Double] ={
   val url = getClass.getResource("/" + path).getPath
   val src = scala.io.Source.fromFile(url)
-  val iter = src.getLines()
-  val result: Map[String, Int] = (for (row <- iter) yield {
-   val seg = row.split("\t"); (seg(0) -> seg(1).toInt)
-  }).toMap
+  val iter = src.getLines().toList
+  val result = sentimentListToMap(iter)
   src.close()
   result
+ }
+
+ def sentimentListToMap(input: List[String]): Map[String,Double] ={
+  input
+     .map{x => x.replaceAll("\\|[A-Z][A-Z]\\s"," ")}
+     .map{x => x.replaceAll(","," ")}
+     .map{x => x.split("\\s")}
+     .map{x => (x(0),x(1),x.tail.tail)}
+     .map{x => (x._2,x._3++List(x._1))}
+     .map(x => (x._2.map(y => (x._1,y))))
+     .flatten
+     .map(x => x.swap)
+     .map(x => (x._1,x._2.toDouble))
+     .toMap
  }
 
  def analyseSentens(data: DataFrame): DataFrame ={
