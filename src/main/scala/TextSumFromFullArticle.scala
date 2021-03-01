46ab1bb201279de6360b51de320e264948581168
Lennart Döring <doering@iplytics.com>
2021-03-01 13:10:30 +0100

    Delete unused Classes and documentation for TextSum

--- src/main/scala/TextSumFromFullArticle.scala
+++ src/main/scala/TextSumFromFullArticle.scala
@@ -4,12 +4,24 @@ import scala.collection.mutable
 
 object TextSumFromFullArticle {
 
+  /**
+   * Get the Data from Sparksession and apply the TextSum algorithm
+   *
+   * @param df
+   * @param spark
+   * @return
+   */
   def getData(df: DataFrame, spark: SparkSession): DataFrame = {
-
     applyTextSum(df, spark)
   }
 
-
+  /**
+   * This methods splits text into sentences by using regex.
+   *
+   * @param input
+   * @return
+   */
+  //TODO: Check if Date formats are identified correctly. e.g. the dot in "17. Januar" should not result into new Sentence. 17. Januar
   def splitSentences(input: String) = {
     //regex splits sentences
     input
@@ -19,6 +31,11 @@ object TextSumFromFullArticle {
       .map(x => x.swap)
   }
 
+  /**
+   * This method tokenizes sentences into tokens.
+   * @param input
+   * @return
+   */
   def tokenize(input: (Int, String)): List[(Int, String)] = {
 
     input._2
@@ -34,6 +51,15 @@ object TextSumFromFullArticle {
 
   }
 
+  /**
+   * This method identifies the three most important sentences in an article. The identification happens through
+   * keywords. Sentences that contain the most keywords are best equipped to represent the content of a text. This makes
+   * this algorithm the easiest way to summarize a text.
+   * @param article
+   * @param keys
+   * @param id
+   * @return
+   */
   def textSum(article: String, keys: List[String], id: String): (String, String) = {
 
     val lowerCaseKeys = keys.map(x => x.toLowerCase)
@@ -57,10 +83,16 @@ object TextSumFromFullArticle {
 
   }
 
+  /**
+   * Apply Method for TextSum
+   * @param data
+   * @param spark
+   * @return
+   */
   def applyTextSum(data: DataFrame, spark: SparkSession): DataFrame = {
 
     val textSum_rdd = data.select("long_url", "text", "keywords_extracted.result").distinct.rdd.map(x => textSum(x.getAs[String](1), x.getAs[mutable.WrappedArray[String]](2).toList, x.getAs[String](0)))
-    val  textSum_df =spark.createDataFrame(textSum_rdd).toDF("long_url", "textSum")
+    val textSum_df =spark.createDataFrame(textSum_rdd).toDF("long_url", "textSum")
 
     textSum_df.join(data, Seq("long_url"), joinType = "outer")
 
